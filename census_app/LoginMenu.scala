791de765d37d3e1cfbdf8fc99878f8f95a8e16de
Michael Celusniak <49047115+mikecelus912@users.noreply.github.com>
2022-06-15 11:45:55 -0400

    Commit 1
    Here is a basic project outline. There is a DatabaseManager class which will create the SparkSession as well as a function for any DataFrames. It also utilizes the DatabaseAnalysis class to map functions contained within that class into a scenario function which maps each DataAnalysis function to its own case which can then be referenced in Main. There is a MenuChoices object with functions that can be called within Main. The Main object utilizes a while loop to control the flow of the application with each case calling either the relevant menu or DataAnalysis function. It is definitely a recursive menu that can be optimized. However, my main focus right now is connecting to Azure within the DatabaseManager class. Tek created the LoginMenu to check for a username/password combination which needs to be incorporated into Main.

---
+++ census_app/LoginMenu.scala
@@ -0,0 +1,14 @@
+package census_app
+
+import scala.Console.println
+import org.apache.spark.sql.SparkSession
+import java.util.InputMismatchException._
+import scala.collection.mutable._
+import scala.collection.JavaConversions._
+import java.util.{InputMismatchException, Scanner}
+
+object LoginMenu {
+
+
+
+}
