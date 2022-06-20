b1e28e69149a3c4ea69497c6390586d27a601da2
Michael Celusniak <49047115+mikecelus912@users.noreply.github.com>
2022-06-20 15:46:43 -0400

    Fully Function Menu Commit
    All menus are contained in a MenuChoices Object which has functions to call login_2 from the Login class, as well as functions for a main_menu, Q1, Q2, Q3, and Q4. The Database Analysis class handles all of the queries which are then mapped to a case within the DatabaseManager class. An instance of databaseManager is then used to run each query from the MenuChoices class. DatabaseManager was also updated with a Timer function that is also utilized after each query to pause the application until a user presses any key.

---
+++ src/main/scala/census_app/DataAnalysis.scala
@@ -0,0 +1,181 @@
+package census_app
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types._
+
+class DataAnalysis {
+
+  def Total_Pops(spark:SparkSession): Unit = {
+    val Max20 = spark.sql("select sum(State_pop) as max_population_2020 from 2020_data")
+    Max20.show()
+
+    val Max10 = spark.sql("select sum(State_pop) as max_population_2010 from 2010_data")
+    Max10.show()
+
+    val Max00 = spark.sql("select sum(State_pop) as max_population_2000 from 2000_data")
+    Max00.show()
+
+  }
+
+  def Region_Pops(spark:SparkSession): Unit = {
+    var year = 2000
+
+    while(year < 2029) {
+      val R100 = spark.sql("select sum(state_pop) as Region_1 from "+year+"_Data where Region = '1'")
+      var temp = R100.select("Region_1").collect().toList
+      val R100i = temp(0).toString().substring(1, temp(0).toString().length - 1).toInt
+
+      val R200 = spark.sql("select sum(state_pop) as Region_2 from "+year+"_Data where Region = '2'")
+      temp = R200.select("Region_2").collect().toList
+      val R200i = temp(0).toString().substring(1, temp(0).toString().length - 1).toInt
+
+      val R300 = spark.sql("select sum(state_pop) as Region_3 from "+year+"_Data where Region = '3'")
+      temp = R300.select("Region_3").collect().toList
+      val R300i = temp(0).toString().substring(1, temp(0).toString().length - 1).toInt
+
+      val R400 = spark.sql("select sum(state_pop) as Region_4 from "+year+"_Data where Region = '4'")
+      temp = R400.select("Region_4").collect().toList
+      val R400i = temp(0).toString().substring(1, temp(0).toString().length - 1).toInt
+
+      println(year)
+      if (R100i > R200i) {
+        if (R300i > R400i) {
+          if (R100i > R300i) {
+            R100.show()
+          } else {
+            R300.show()
+          }
+        } else if (R100i > R400i) {
+          R100.show()
+        } else {
+          R400.show()
+        }
+      } else if (R300i > R400i) {
+        if (R200i > R300i) {
+          R200.show()
+        } else {
+          R300.show()
+        }
+      } else if (R200i > R400i) {
+        R200.show()
+      } else {
+        R400.show()
+      }
+      year = year + 10
+    }
+  }
+
+  def Race_Pops(spark:SparkSession): Unit = {
+    var year = 2000
+    while(year < 2029) {
+      val Race00 = spark.sql("Select sum(WO),sum(BO),sum(AIO),sum(AO),sum(NHO),sum(SOR),sum(HL) from " + year + "_data")
+      println(year)
+      Race00.show()
+      year = year + 10
+    }
+  }
+
+  def Region_Deff(spark:SparkSession): Unit = {
+    var year = 2000
+    //var temp = ListBuffer[String]()
+    val temp = scala.collection.mutable.Map[String, Int]()
+    while (year < 2019) {
+      val year10 = year + 10
+      var i = 1
+      while(i<5) {
+        val Deff00_10_1 = spark.sql("Select(select sum(state_pop) from " + year10 + "_data where Region = "+i+")" +
+          "-" +
+          "(select sum(state_pop) from " + year + "_data where Region = "+i+") as 2000_to_2010")
+        var temp1 = Deff00_10_1.select("2000_to_2010").collect().toList.toString()
+        temp += s"${year}_to_${year10}_$i" -> temp1.substring(6, temp1.length - 2).toInt
+        i=i+1
+      }
+      year = year10
+    }
+    var i = 0
+    while(i<3) {
+      val max = temp.valuesIterator.max
+      var key = temp.filter(_._2 == max).toString()
+      val temp1 = key.split(" ")
+      key = temp1(0).substring(8)
+      println(i+1+". Years and Region: "+key+" Difference: "+max)
+      temp.remove(key)
+      i=i+1
+    }
+  }
+
+  def State_Deff(spark:SparkSession): Unit = {
+    var year = 2000
+    //var temp = ListBuffer[String]()
+    val temp = scala.collection.mutable.Map[String, Int]()
+    while (year < 2019) {
+      val year10 = year + 10
+      var i = 1
+      while(i<77) {
+        val Deff00_10_1 = spark.sql("Select(select state_pop from " + year10 + "_data where State_no = "+i+")" +
+          "-" +
+          "(select state_pop from " + year + "_data where State_no = "+i+") as 2000_to_2010")
+        var temp1 = Deff00_10_1.select("2000_to_2010").collect().toList.toString()
+        val temp2 = temp1.substring(6,temp1.length-2)
+        if(temp2.equals("null")){
+          //     println("No state with that number")
+        }else {
+          var Name = spark.sql("Select State_Name from "+year+"_data where State_no = "+i)
+          temp1 = Name.select("State_Name").collect().toList.toString()
+          val SName = temp1.substring(6,temp1.length-2)
+          temp += s"${year}_to_${year10}_$SName" -> temp2.toInt
+        }
+        i=i+1
+      }
+      year = year10
+    }
+    var i = 0
+    while(i<10) {
+      val max = temp.valuesIterator.max
+      var key = temp.filter(_._2 == max).toString()
+      val temp1 = key.split(" ")
+      key = temp1(0).substring(8)
+      println(i+1+".\tYears and State: "+key+"\tDifference: "+max)
+      temp.remove(key)
+      i=i+1
+    }
+  }
+
+  def decr_pops(spark:SparkSession): Unit = {
+    var year = 2000
+    //var temp = ListBuffer[String]()
+    val temp = scala.collection.mutable.Map[String, Int]()
+    while (year < 2019) {
+      val year10 = year + 10
+      var i = 1
+      while (i < 77) {
+        val Deff00_10_1 = spark.sql("Select(select state_pop from " + year10 + "_data where State_no = " + i + ")" +
+          "-" +
+          "(select state_pop from " + year + "_data where State_no = " + i + ") as 2000_to_2010")
+        var temp1 = Deff00_10_1.select("2000_to_2010").collect().toList.toString()
+        val temp2 = temp1.substring(6, temp1.length - 2)
+        if (temp2.equals("null")) {
+          //     println("No state with that number")
+        } else if (temp2.toInt < 0) {
+          var Name = spark.sql("Select State_Name from " + year + "_data where State_no = " + i)
+          temp1 = Name.select("State_Name").collect().toList.toString()
+          val SName = temp1.substring(6, temp1.length - 2)
+          temp += s"${year}_to_${year10}_$SName" -> temp2.toInt
+        }
+        i = i + 1
+      }
+      year = year10
+    }
+    var i = 0
+    while (i < 6) {
+      val max = temp.valuesIterator.min
+      var key = temp.filter(_._2 == max).toString()
+      val temp1 = key.split(" ")
+      key = temp1(0).substring(8)
+      println(i + 1 + ".\tYears and State: " + key + "\tDifference: " + max)
+      temp.remove(key)
+      i = i + 1
+    }
+  }
+}
---
+++ src/main/scala/census_app/Login.scala
@@ -0,0 +1,129 @@
+package census_app
+
+import scala.Console.println
+import scala.collection.mutable.Map
+import org.apache.spark.sql.SparkSession
+
+class Login() {
+
+  val databaseManager = new DatabaseManager()
+
+
+  def createLoginTable(): Unit = {
+    val loginDB = databaseManager.spark.read.options(Map("inferSchema" -> "true", "header" -> "true"))
+      .csv("src/input/logintable")
+    loginDB.createOrReplaceTempView("logins")
+  }
+
+  def login(): Unit = {
+    println("Please enter your username: ")
+    val username = scala.io.StdIn.readLine()
+    val loginDB = databaseManager.spark.read.options(Map("inferSchema" -> "true", "header" -> "true"))
+      .csv("src/input/logintable").toDF()
+    loginDB.createOrReplaceTempView("logins")
+    val first1 = databaseManager.spark.sql("SELECT username FROM logins WHERE username = '" + username + "'")
+    if (first1.count() == 1) {
+      println("Please enter your password: ")
+      val userpassword = scala.io.StdIn.readLine()
+      val first2 = databaseManager.spark.sql("SELECT password FROM logins WHERE password = '" + userpassword + "'")
+      if (first2.count() == 1) {
+        println("Login successful!\n")
+        MenuChoices.main_menu()
+      }
+      else {
+        println("Incorrect password! Please try again.")
+        print("Enter password again: ")
+        val userpassword = scala.io.StdIn.readLine()
+        val first3 = databaseManager.spark.sql("SELECT password FROM logins WHERE password = '" + userpassword + "'")
+        if (first3.count() == 1) {
+          println("Login successful!\n")
+          MenuChoices.main_menu()
+        }
+        else {
+          println("You have entered the incorrect password too many times.\n")
+          MenuChoices.login_menu()
+        }
+      }
+    }
+    else {
+      println("Incorrect username, please try again!\n")
+      MenuChoices.login_menu()
+    }
+  }
+
+  def insert(): Unit = {
+    print("Create new username: ")
+    val username = scala.io.StdIn.readLine()
+    val loginDB = databaseManager.spark.read.options(Map("inferSchema" -> "true", "header" -> "true"))
+      .csv("src/input/logintable").toDF()
+    loginDB.createOrReplaceTempView("logins")
+    databaseManager.spark.sql("CREATE table login2 AS SELECT * FROM logins")
+    val step1 = databaseManager.spark.sql("SELECT username FROM login2 WHERE username = '" + username + "'")
+    if (step1.count() > 0) {
+      println(username + " Username already exists! Please try another username")
+    }
+    else {
+      print("Create password: ")
+      val userpassword = scala.io.StdIn.readLine()
+      print("Confirm password: ")
+      val password_confirm = scala.io.StdIn.readLine()
+      if (userpassword == password_confirm) {
+        databaseManager.spark.sql("INSERT into login2(username, password) VALUES('" + username + "','" + userpassword + "')")
+        println("Login created successfully.")
+      }
+      else {
+        println("Sorry, password didn't match.")
+      }
+    }
+  }
+
+  val globallogindata = scala.collection.mutable.Map("tek" -> "1234",
+    "admin" -> "root")
+
+  def login_2(): Unit = {
+    //val logindata = scala.collection.mutable.Map("tek" -> "1234")
+    print("Please enter username: ")
+    val username_in = scala.io.StdIn.readLine()
+    if (globallogindata.contains(username_in)) {
+      print("Please enter password: ")
+      val password = if (globallogindata.contains(username_in)) globallogindata(username_in) else 0
+      val password_in = scala.io.StdIn.readLine()
+      if (password_in == password) {
+        println("Login successful!\n")
+        MenuChoices.main_menu()
+      }
+      else {
+        println("Incorrect password, please try again!\n")
+        MenuChoices.login_menu()
+      }
+    }
+    else {
+      println("Incorrect username: please try again!")
+      MenuChoices.login_menu()
+    }
+  }
+
+  def create(): Unit = {
+    print("Create a temporary username: ")
+    val username_new = scala.io.StdIn.readLine()
+    if (globallogindata.contains(username_new)) {
+      println("Username already exists, please try creating different username!\n")
+      create()
+    }
+    else {
+      print("Create a new temporary password: ")
+      val password_new = scala.io.StdIn.readLine()
+      print("Confirm password: ")
+      val password_confirm = scala.io.StdIn.readLine()
+      if (password_confirm == password_new) {
+        globallogindata += (username_new -> password_new)
+        println("Logging in as guest with temporary credentials!")
+        MenuChoices.main_menu()
+      }
+      else {
+        println("Sorry, password didn't match!")
+        MenuChoices.login_menu()
+      }
+    }
+  }
+}
